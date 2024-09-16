6866804d29e12044124c429eb4b832f18b6930f7
Youenn <156934138+Monsteryou@users.noreply.github.com>
2024-09-16 11:18:25 +0200

    Doxygen

--- sensor.h
+++ sensor.h
@@ -1,22 +1,57 @@
-#ifndef "sensor_h"
-#define "sensor_h"
+#ifndef SENSOR_H
+#define SENSOR_H
 
 using namespace std;
 
-class sensor
+/**
+ * @brief Classe représentant un capteur de température.
+ * 
+ * La classe Sensor permet de lire et d'afficher la température d'un capteur. Elle propose
+ * un constructeur par défaut, un constructeur avec une adresse de capteur, et un destructeur.
+ */
+class Sensor
 {  
+public:
+    /**
+     * @brief Constructeur par défaut de la classe Sensor.
+     * 
+     * Ce constructeur initialise l'attribut `Temp` à 0.0 et prépare l'objet Sensor.
+     */
+    Sensor();
 
-Public:
-		sensor();
-		sensor(int adrTemp);
-		~sensor();
-	void lireTemp();
-	void affTemp();
-Private:
+    /**
+     * @brief Constructeur avec paramètre.
+     * 
+     * Ce constructeur prend en paramètre une adresse du capteur, typiquement une adresse I2C,
+     * pour initialiser un capteur à une adresse spécifique.
+     * 
+     * @param adrTemp Adresse du capteur (ex. adresse I2C).
+     */
+    Sensor(int adrTemp);
 
-	float Temp;
+    /**
+     * @brief Destructeur de la classe Sensor.
+     * 
+     * Libère les ressources utilisées par l'objet Sensor.
+     */
+    ~Sensor();
 
+    /**
+     * @brief Méthode pour lire la température.
+     * 
+     * Simule la lecture de la température depuis un capteur et met à jour l'attribut `Temp`.
+     */
+    void lireTemp();
 
-}
+    /**
+     * @brief Méthode pour afficher la température.
+     * 
+     * Affiche la valeur actuelle de la température stockée dans `Temp`.
+     */
+    void affTemp();
 
-endif
+private:
+    float Temp; /**< @brief Stocke la température lue par le capteur. */
+};
+
+#endif
